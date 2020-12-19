3943e8103ef917e13794c05826a13bb1607453e1
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2020-12-19 21:39:13 +0530

    [Introduction]feature Updated SVG template and checking for working

--- src/main/java/com/github/component/GithubProfileComponents/Controller/GitIntroController.java
+++ src/main/java/com/github/component/GithubProfileComponents/Controller/GitIntroController.java
@@ -1,14 +1,24 @@
 package com.github.component.GithubProfileComponents.Controller;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/github/introduction")
 public class GitIntroController {
-    @RequestMapping("/public")
+    @GetMapping("/public")
     public String getStatistics() {
-        return "introduction/index";
+        return "<svg width=\"700px\" height=\"40px\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                "   <foreignObject width=\"100%\" height=\"100%\">\n" +
+                "     <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
+                "         <style>\n" +
+                "            p{\n" +
+                "                color: red;\n" +
+                "                }\n" +
+                "         </style>\n" +
+                "         <p>hi</p>\n" +
+                "     </div>\n" +
+                "   </foreignObject>\n" +
+                "</svg>";
     }
 }
