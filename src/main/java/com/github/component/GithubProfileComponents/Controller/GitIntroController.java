5a09d4b9d2716e3b33b8da815fa023f52f2c4e76
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2020-12-19 22:07:13 +0530

    [Introduction]feature Updated SVG template and checking for working

--- src/main/java/com/github/component/GithubProfileComponents/Controller/GitIntroController.java
+++ src/main/java/com/github/component/GithubProfileComponents/Controller/GitIntroController.java
@@ -8,17 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class GitIntroController {
     @GetMapping("/public")
     public String getStatistics() {
-        return "<svg width=\"700px\" height=\"40px\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
+        return "<svg width=\"700px\" height=\"150px\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
                 "   <foreignObject width=\"100%\" height=\"100%\">\n" +
                 "     <div xmlns=\"http://www.w3.org/1999/xhtml\">\n" +
                 "         <style>\n" +
-                "            p{\n" +
-                "                color: red;\n" +
-                "                }\n" +
+                "            .card {\n" +
+                "                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);\n" +
+                "                transition: 0.3s;\n" +
+                "                width: 40%;\n" +
+                "            }\n" +
+                "            .card:hover {\n" +
+                "                box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);\n" +
+                "            }\n" +
+                "            .container {\n" +
+                "              padding: 2px 16px;\n" +
+                "            }\n" +
                 "         </style>\n" +
-                "         <p>hi</p>\n" +
+                "         <div class=\"card\">\n" +
+                "             <div class=\"container\">\n" +
+                "                 <h4><b>John Doe</b></h4>\n" +
+                "                 <p>Architect & Engineer</p>\n" +
+                "             </div>\n" +
+                "         </div>\n" +
                 "     </div>\n" +
                 "   </foreignObject>\n" +
-                "</svg>";
+                "</svg>\n" +
+                "\n";
     }
 }
